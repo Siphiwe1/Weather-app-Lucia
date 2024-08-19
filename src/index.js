d9974cdf72138fb8687fb9d010e169cf79852e4f
Siphiwe1 <159583745+Siphiwe1@users.noreply.github.com>
2024-08-19 14:58:05 +0200

    added the API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+function refreshWeather(response){
+    let temperatureElement= document.querySelector("#temperature");
+    let temperature= response.data.temperature.current;
+
+    cityElement.innerHTML=response.data.city;
+    temperatureElement.innerHTML=Math.round(temperature);
+  
+}
+
+
+
+function searchCity (city){
+    let apiKey = "4bef39f46ffb7o39t6446dac03e1804a";
+    let apiURL =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let SearchInput= document.querySelector("#Search-form-Input");
@@ -8,4 +26,5 @@ function handleSearchSubmit(event){
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Springs");
 
